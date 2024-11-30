9ab9ad06ec35821c8d7939bc68f84560903f6cce
ArtemBurenok <burenok-02@mail.ru>
2024-11-30 15:45:26 +0300

    Update create_table.sql

--- progect/create_table.sql
+++ progect/create_table.sql
@@ -1,27 +1,27 @@
-CREATE TABLE StockDim (
-    StockID SERIAL PRIMARY KEY,
-    Ticker VARCHAR(10) NOT NULL,
-    CompanyName VARCHAR(100) NOT NULL,
-    Sector VARCHAR(50),
-    Industry VARCHAR(50),
+CREATE TABLE stockDim (
+    stockID SERIAL PRIMARY KEY,
+    ticker VARCHAR(10) NOT NULL,
+    companyName VARCHAR(100) NOT NULL,
+    sector VARCHAR(50),
+    industry VARCHAR(50),
     ISIN VARCHAR(12)
 );
 
-CREATE TABLE TraderDim (
-    TraderID SERIAL PRIMARY KEY,
-    TraderName VARCHAR(100) NOT NULL,
-    AccountType VARCHAR(50),
-    RegisterDate DATE,
-    Location VARCHAR(100)
+CREATE TABLE traderDim (
+    traderID SERIAL PRIMARY KEY,
+    traderName VARCHAR(100) NOT NULL,
+    accountType VARCHAR(50),
+    registerDate DATE,
+    location VARCHAR(100)
 );
 
-CREATE TABLE Trades (
-    TradeID SERIAL PRIMARY KEY,
-    StockID INT NOT NULL,
-    TraderID INT NOT NULL,
-    Quantity INT NOT NULL,
-    Price DECIMAL(10, 2) NOT NULL,
-    TotalValue DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (StockID) REFERENCES StockDim(StockID),
-    FOREIGN KEY (TraderID) REFERENCES TraderDim(TraderID)
+CREATE TABLE trades (
+    tradeID SERIAL PRIMARY KEY,
+    stockID INT NOT NULL,
+    traderID INT NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    totalValue DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (stockID) REFERENCES stockDim(stockID),
+    FOREIGN KEY (traderID) REFERENCES traderDim(traderID)
 );
