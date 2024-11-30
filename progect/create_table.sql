da7454dd47eba8e00756e62b15bc9386859da20f
ArtemBurenok <burenok-02@mail.ru>
2024-11-30 15:56:15 +0300

    Clean code, uncle Bob

--- progect/create_table.sql
+++ progect/create_table.sql
@@ -1,27 +1,27 @@
-CREATE TABLE stockDim (
-    stockID SERIAL PRIMARY KEY,
+CREATE TABLE stock_dim (
+    stock_id SERIAL PRIMARY KEY,
     ticker VARCHAR(10) NOT NULL,
-    companyName VARCHAR(100) NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
     sector VARCHAR(50),
     industry VARCHAR(50),
     ISIN VARCHAR(12)
 );
 
-CREATE TABLE traderDim (
-    traderID SERIAL PRIMARY KEY,
-    traderName VARCHAR(100) NOT NULL,
-    accountType VARCHAR(50),
-    registerDate DATE,
+CREATE TABLE trader_dim (
+    trader_id SERIAL PRIMARY KEY,
+    trader_name VARCHAR(100) NOT NULL,
+    account_type VARCHAR(50),
+    register_date DATE,
     location VARCHAR(100)
 );
 
 CREATE TABLE trades (
-    tradeID SERIAL PRIMARY KEY,
-    stockID INT NOT NULL,
-    traderID INT NOT NULL,
+    trade_id SERIAL PRIMARY KEY,
+    stock_id INT NOT NULL,
+    trader_id INT NOT NULL,
     quantity INT NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
-    totalValue DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (stockID) REFERENCES stockDim(stockID),
-    FOREIGN KEY (traderID) REFERENCES traderDim(traderID)
+    total_value DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (stock_id) REFERENCES stock_dim(stock_id),
+    FOREIGN KEY (trader_id) REFERENCES trader_dim(trader_id)
 );
--- progect/task1.sql
+++ progect/task1.sql
@@ -1,32 +1,32 @@
 -- Информация о сделках с данными о трейдерах и акциях
-SELECT t.tradeID, s.ticker, tr.traderName, t.quantity, t.price, t.totalValue
-FROM trades t JOIN stockDim s ON t.stockID = s.stockID
-	 JOIN traderDim tr ON t.traderID = tr.traderID
+SELECT t.trade_id, s.ticker, tr.traderName, t.quantity, t.price, t.total_value
+FROM trades t JOIN stock_dim s ON t.stock_id = s.stock_id
+	 JOIN trader_dim tr ON t.trader_id = tr.trader_id
 LIMIT 10;
 
 -- Общая сумма сделок для каждого трейдера
-SELECT tr.traderName, SUM(t.totalValue) OVER (PARTITION BY tr.traderID) AS CumulativeTotalValue
-FROM trades t JOIN traderDim tr ON t.traderID = tr.traderID
+SELECT tr.trader_name, SUM(t.total_value) OVER (PARTITION BY tr.trader_id) AS CumulativeTotalValue
+FROM trades t JOIN trader_dim tr ON t.trader_id = tr.trader_id
 ORDER BY CumulativeTotalValue
 LIMIT 10;
 
 -- Общее количество сделок и общая стоимость сделок по каждому трейдеру
-SELECT tr.traderName, COUNT(t.tradeID) AS TotalTrades, SUM(t.totalValue) AS TotalTradeValue
-FROM trades t JOIN traderDim tr ON t.traderID = tr.traderID
-GROUP BY tr.traderName
+SELECT tr.trader_name, COUNT(t.trade_id) AS TotalTrades, SUM(t.total_value) AS TotalTradeValue
+FROM trades t JOIN trader_dim tr ON t.trader_id = tr.trader_id
+GROUP BY tr.trader_name
 LIMIT 10
 
 -- Трейдеры, которые совершили более 2 сделок
-SELECT tr.traderName, COUNT(t.tradeID) AS TotalTrades
-FROM trades t JOIN traderDim tr ON t.traderID = tr.traderID
-GROUP BY tr.traderName
-HAVING COUNT(t.tradeID) > 2
+SELECT tr.trader_name, COUNT(t.trade_id) AS TotalTrades
+FROM trades t JOIN trader_dim tr ON t.trader_id = tr.trader_id
+GROUP BY tr.trader_name
+HAVING COUNT(t.trade_id) > 2
 LIMIT 10;
 
 -- Список всех трейдеров и акций
-SELECT traderName AS Name, 'Trader'
-FROM traderDim
+SELECT trader_name AS Name, 'Trader'
+FROM trader_dim
 UNION
-SELECT companyName AS Name, 'Stock'
-FROM stockDim
+SELECT company_name AS Name, 'Stock'
+FROM stock_dim
 LIMIT 10;
