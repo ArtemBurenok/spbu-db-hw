e7a5f56e83f393e4f17670d217a5292f4b33c59b
ArtemBurenok <burenok-02@mail.ru>
2024-11-30 16:28:51 +0300

    add previous changes

--- progect/task2.sql
+++ progect/task2.sql
@@ -1,59 +1,59 @@
 -- Временные таблицы
 
-CREATE TEMPORARY TABLE TemporaryStockDim (
-    Ticker VARCHAR(10) NOT NULL,
-    CompanyName VARCHAR(100) NOT NULL,
-    Sector VARCHAR(50),
-    Industry VARCHAR(50),
+CREATE TEMPORARY TABLE temporary_stock_dim (
+    ticker VARCHAR(10) NOT NULL,
+    company_name VARCHAR(100) NOT NULL,
+    sector VARCHAR(50),
+    industry VARCHAR(50),
     ISIN VARCHAR(12)
 );
 
-CREATE TEMPORARY TABLE TemporaryTraderDim (
-    TraderName VARCHAR(100) NOT NULL,
-    AccountType VARCHAR(50),
-    RegisterDate DATE,
-    Location VARCHAR(100)
+CREATE TEMPORARY TABLE temporary_trader_dim (
+    trader_name VARCHAR(100) NOT NULL,
+    account_type VARCHAR(50),
+    register_date DATE,
+    location VARCHAR(100)
 );
 
-CREATE TEMPORARY TABLE TemporaryTrades (
-    StockID INT NOT NULL,
-    TraderID INT NOT NULL,
-    Quantity INT NOT NULL CHECK (Quantity > 0),
-    Price DECIMAL(10, 2) NOT NULL CHECK (Price >= 0),
-    TotalValue DECIMAL(10, 2) NOT NULL CHECK (TotalValue >= 0)
+CREATE TEMPORARY TABLE temporary_trades (
+    stock_id INT NOT NULL,
+    trader_id INT NOT NULL,
+    quantity INT NOT NULL CHECK (quantity > 0),
+    price DECIMAL(10, 2) NOT NULL CHECK (price >= 0),
+    total_value DECIMAL(10, 2) NOT NULL CHECK (total_value >= 0)
 );
 
 -- Представления с данными, которые были получены путём объединения всех таблиц
 
-CREATE VIEW TradeDetails AS
-SELECT t.TradeID, s.Ticker, s.CompanyName, tr.TraderName, t.Quantity, t.Price, t.TotalValue
-FROM Trades t JOIN StockDim s ON t.StockID = s.StockID
-	JOIN TraderDim tr ON t.TraderID = tr.TraderID;
+CREATE VIEW trade_details AS
+SELECT t.trade_id, s.ticker, s.company_name, tr.trader_name, t.quantity, t.price, t.total_value
+FROM trades t JOIN stock_dim s ON t.stock_id = s.stock_id
+	JOIN trader_dim tr ON t.trader_id = tr.trader_id;
 
 
 
-WITH TradeSummary AS (
-    SELECT t.TraderID, s.Ticker, SUM(t.Quantity) AS TotalQuantity, SUM(t.TotalValue) AS TotalValue
-    FROM Trades t JOIN StockDim s ON t.StockID = s.StockID
-    GROUP BY t.TraderID, s.Ticker
+WITH trade_summary AS (
+    SELECT t.trader_id, s.ticker, SUM(t.quantity) AS TotalQuantity, SUM(t.total_value) AS TotalValue
+    FROM trades t JOIN stock_dim s ON t.stock_id = s.stock_id
+    GROUP BY t.trader_id, s.ticker
 ),
 
-TraderDetails AS (
-    SELECT tr.TraderID, tr.TraderName, tr.AccountType, tr.Location
-    FROM TraderDim tr
+trader_details AS (
+    SELECT tr.trader_id, tr.trader_name, tr.account_type, tr.location
+    FROM trader_dim tr
 )
 
-SELECT td.TraderName, td.AccountType, ts.Ticker, ts.TotalQuantity, ts.TotalValue
-FROM TradeSummary ts JOIN TraderDetails td ON ts.TraderID = td.TraderID
-ORDER BY td.TraderName, ts.Ticker;
+SELECT td.trader_name, td.account_type, ts.ticker, ts.TotalQuantity, ts.TotalValue
+FROM trade_summary ts JOIN trader_details td ON ts.trader_id = td.trader_id
+ORDER BY td.trader_name, ts.ticker;
 
 -- Ограничения на уровне таблиц
 
-ALTER TABLE Trades
-ADD CONSTRAINT chk_Quantity CHECK (Quantity > 0);
+ALTER TABLE trades
+ADD CONSTRAINT check_Quantity CHECK (Quantity > 0);
 
-ALTER TABLE Trades
-ADD CONSTRAINT chk_Price CHECK (Price >= 0);
+ALTER TABLE trades
+ADD CONSTRAINT check_Price CHECK (Price >= 0);
 
 -- Триггер для валидации
 
@@ -61,13 +61,13 @@ CREATE OR REPLACE FUNCTION validate_trade()
 RETURNS TRIGGER AS $$
 BEGIN
     -- Проверка на существование StockID
-    IF NOT EXISTS (SELECT 1 FROM StockDim WHERE StockID = NEW.StockID) THEN
-        RAISE EXCEPTION 'StockID % does not exist', NEW.StockID;
+    IF NOT EXISTS (SELECT 1 FROM stock_dim WHERE stock_id = NEW.stock_id) THEN
+        RAISE EXCEPTION 'StockID % does not exist', NEW.stock_id;
     END IF;
 
     -- Проверка на существование TraderID
-    IF NOT EXISTS (SELECT 1 FROM TraderDim WHERE TraderID = NEW.TraderID) THEN
-        RAISE EXCEPTION 'TraderID % does not exist', NEW.TraderID;
+    IF NOT EXISTS (SELECT 1 FROM trader_dim WHERE trade_id = NEW.trade_id) THEN
+        RAISE EXCEPTION 'TraderID % does not exist', NEW.trade_id;
     END IF;
 
     RETURN NEW;
@@ -75,5 +75,5 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER trg_validate_trade
-BEFORE INSERT OR UPDATE ON Trades
+BEFORE INSERT OR UPDATE ON trades
 FOR EACH ROW EXECUTE FUNCTION validate_trade();
